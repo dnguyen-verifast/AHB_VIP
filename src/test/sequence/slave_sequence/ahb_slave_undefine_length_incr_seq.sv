0e6e495b46bfba126daee74d36f950f3d4e99e61
dnguyen-verifast <dnguyen@verifasttech.com>
2026-06-01 23:21:27 +0700

    fcommit

--- src/test/sequence/slave_sequence/ahb_slave_undefine_length_incr_seq.sv
+++ src/test/sequence/slave_sequence/ahb_slave_undefine_length_incr_seq.sv
@@ -24,7 +24,7 @@ task ahb_slave_undefine_length_incr_seq::body();
     end
     $cast(cloned_req,req_slv.clone());
     p_sequencer.seq_expect_item_port.write(cloned_req);
-    `uvm_info("slave_seq",$sformatf("req_slv = %s", req_slv.sprint()))
+    `uvm_info("slave_seq",$sformatf("req_slv = %s", req_slv.sprint()),UVM_LOW)
     finish_item(req_slv);
 endtask : body
 
